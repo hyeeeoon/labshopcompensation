202d859c0e730b6965206f119e71532b1f46fbbb
hyeeeoon na <423nagi@gmail.com>
2025-06-13 06:08:17 +0000

    +

--- frontend/src/components/Order.vue
+++ frontend/src/components/Order.vue
@@ -23,6 +23,8 @@
             <Number label="Amount" v-model="value.amount" :editMode="editMode"/>
             <String label="Status" v-model="value.status" :editMode="editMode"/>
             <String label="Address" v-model="value.address" :editMode="editMode"/>
+
+            <Inventory v-model="inventory"></Inventory>
         </v-card-text>
 
         <v-card-actions>
@@ -99,7 +101,12 @@
                 timeout: 5000,
                 text: ''
             },
+            inventory: null
         }),
+        async created(){
+            var result = await axios.get('/inventories/' + this.value.productId);
+            this.inventory = result.data;
+        },
         computed:{
         },
         methods: {
